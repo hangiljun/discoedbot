88836e31fa2c790a736d79fbcce76f45f673353e
길준사이트 <myrlfwns1@naver.com>
2026-05-05 17:56:15 +0900

    feat: 게임역할패널 embed 문구 업데이트

--- bot.py
+++ bot.py
@@ -1998,7 +1998,13 @@ async def game_role_panel(interaction: discord.Interaction):
         return
     embed = discord.Embed(
         title="서버 역할 선택",
-        description="메이플 본서버 유저는 메이플 본서버 인증 해주세요.\n메이플클래식 유저 해당서버 버튼 클릭",
+        description=(
+            "- 🍁 Maple Story Discord 🍁 -\n\n"
+            "✅ 지금 보는 공지사항 채널은 메이플 클래식 채널입니다.\n\n"
+            "🍁 메이플클래식(플래닛/랜드)유저분들은 하단에 필요한 역할 클릭\n"
+            "   -> 클릭을 하셔야 정상적인 채널 이용이 가능합니다.\n\n"
+            "디스코드 초대링크 : <https://discord.gg/2UwBw8dnSv>"
+        ),
         color=discord.Color.green()
     )
     await channel.send(embed=embed, view=GameRoleView(guild_id=interaction.guild.id))
